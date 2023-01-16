d1df43feb5c46d598e4016f4e01ddf0082f983a4
Sensei Tarzan <gabrielcaptari0@gmail.com>
2023-01-16 01:41:31 +0100

    fix crash subcommandget params plugin

--- src/SenseiTarzan/RoleManager/Commands/RoleCommands.php
+++ src/SenseiTarzan/RoleManager/Commands/RoleCommands.php
@@ -20,13 +20,13 @@ class RoleCommands extends BaseCommand
     protected function prepare(): void
     {
         $this->setPermission("command.role.permission");
-        $this->registerSubCommand(new setRoleCommands("setrole"));
-        $this->registerSubCommand(new setNameRoleCustomCommands("setnamecustom"));
-        $this->registerSubCommand(new setPrefixCommands("setprefix"));
-        $this->registerSubCommand(new setSuffixCommands("setsuffix"));
-        $this->registerSubCommand(new setPermissionsCommands("setperm"));
-        $this->registerSubCommand(new addPermissionsCommands("addperm"));
-        $this->registerSubCommand(new subPermissionsCommands("subperm"));
+        $this->registerSubCommand(new setRoleCommands($this->plugin, "setrole"));
+        $this->registerSubCommand(new setNameRoleCustomCommands($this->plugin, "setnamecustom"));
+        $this->registerSubCommand(new setPrefixCommands($this->plugin, "setprefix"));
+        $this->registerSubCommand(new setSuffixCommands($this->plugin, "setsuffix"));
+        $this->registerSubCommand(new setPermissionsCommands($this->plugin, "setperm"));
+        $this->registerSubCommand(new addPermissionsCommands($this->plugin, "addperm"));
+        $this->registerSubCommand(new subPermissionsCommands($this->plugin, "subperm"));
     }
 
     public function onRun(CommandSender $sender, string $aliasUsed, array $args): void
